83d187de2b373d838802bbcdf930a4af6e3a2b40
Silvio Wangler <silvio.wangler@gmail.com>
2016-10-27 19:07:23 +0200

    Query Google for Geb Framework

--- src/test/groovy/ch/silviowangler/geb/GoogleSpec.groovy
+++ src/test/groovy/ch/silviowangler/geb/GoogleSpec.groovy
@@ -13,4 +13,26 @@ class GoogleSpec extends GebReportingSpec {
     then:
     title == 'Google'
   }
+
+  void "Make sure the query field is initially empty"() {
+
+    expect:
+    $("input", name: "q").text() == ''
+  }
+
+  void "Enter a query"() {
+
+    when: 'Enter "Geb Framework" into the search field'
+    $("input", name: "q").value 'Geb Framework'
+
+    and: 'Click the search button'
+    $("button", name: "btnG").click()
+
+    and: 'wait until the search result element is visible'
+    waitFor { $('#sbfrm_l').displayed }
+
+    then:
+    $('h3.r')[0].text() == 'Geb - Very Groovy Browser Automation'
+
+  }
 }
